823fa3a3407c6449ce3f4f9c0df773d3d2dac308
MGV <michaeldoestech@gmail.com>
2018-04-15 20:00:22 -0400

    start static server as well

--- bin/start-production.sh
+++ bin/start-production.sh
@@ -2,7 +2,12 @@
 export NODE_ENV=production
 export NODE_EXEC_MODE="CLUSTER,PM2"
 APP_NAME=$1
+STATIC_APP_NAME=$2
 if [ "$APP_NAME" == "" ]; then
 	APP_NAME="fxjsprodapp"
 fi
+if [ "$STATIC_APP_NAME" == "" ]; then
+	STATIC_APP_NAME="fxjsprodapp-static"	
+fi
 ./node_modules/.bin/pm2 start bin/server.js --name="${APP_NAME}"
+./node_modules/.bin/pm2 start bin/static-server.js --name="${STATIC_APP_NAME}
